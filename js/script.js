24949643e37558b98d962177c0433508fc8d1874
Karolina Karczewska <karolcia_yahoo94@wp.pl>
2022-01-02 00:00:44 +0100

    Add active to this

--- js/script.js
+++ js/script.js
@@ -9,6 +9,8 @@
   const titleClickHandler = function(event){
     console.log('Link was clicked!', event);
   
+    const clickedElement = this;
+
     /* remove class 'active' from all article links  */
     const activeLinks = document.querySelectorAll('.titles a.active');
 
@@ -17,6 +19,8 @@
     }
   
     /* add class 'active' to the clicked link */
+    clickedElement.classList.add('active');
+    console.log('clickedElement:', clickedElement);
   
     /* remove class 'active' from all articles */
     const activeArticles = document.querySelectorAll('.post');
